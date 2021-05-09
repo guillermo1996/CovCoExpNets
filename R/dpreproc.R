151ee4ec65db644653d26e35eeb84c24994594b9
carmen-maria-hernandez <carmenmaria.hernandezp@um.es>
2021-05-09 11:18:36 +0200

    Remove redundant predictors function

--- R/dpreproc.R
+++ R/dpreproc.R
@@ -1,4 +1,5 @@
 #' Normalize a vector
+#'
 #' Normalizes a vector by subtracting its mean and dividing by its standard deviation
 #'
 #' @param x numeric vector
@@ -16,6 +17,7 @@ normalize <- function(x){
 }
 
 #' Scaling, centering and normalizing a matrix
+#'
 #' Scales a non-negative numerical matrix by applying the logarithm function
 #' and centers and normalizes its data. When applying the logarithm function
 #' it adds one to avoid calculating the logarithm of zero.
@@ -28,10 +30,41 @@ normalize <- function(x){
 #' @examples
 #' m <- matrix(1:4, nr= 2, nc = 2)
 #' colnames(m)<-c("1", "2")
-#' scn(matrix(1:4, nr= 2, nc = 2))
+#' scn(m)
 scn <- function(data){
   data <- log2(data + 1)
   preProc <- caret:: preProcess(data, method = c("center", "scale"))
-  data <- predict(preProc, data)
+  data <- stats::predict(preProc, data)
   return(data)
 }
+
+#' Remove redundant predictors
+#'
+#' Reduces the number of predictors by eliminating those with near-zero variance
+#' and from pairs that have high correlation (>= 90%) eliminate one of the two predictors.
+#'
+#' @param data numerical matrix with predictors as rows and samples as columns
+#'
+#' @return numerical matrix
+#' @export
+#'
+#' @examples
+#' nycfl = nycflights13::flights
+#' # We won´t use those non numerical predictors
+#' nycfl = na.omit(nycfl[,-c(1,10,12,13,14,19)])
+#' ncol(nycfl)
+#' nycfl2 <- t(rRedundantPredictors(t(nycfl)))
+#' ncol(nycfl2)
+rRedundantPredictors <- function(data){
+  r.data <- apply(data, 1, round)
+  ind <-caret::nearZeroVar(r.data,freqCut = 20, uniqueCut = 5)
+  if(length(ind) > 0)
+    data <- data[-ind,]
+  correlacion <- stats::cor(t(data))
+  eliminar <- caret::findCorrelation(correlacion, cutoff=0.9)
+  data <- data[-eliminar,]
+  return(data)
+}
+
+
+
